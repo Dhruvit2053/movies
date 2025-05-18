5622599751823d2de11fb1379d7969e0e3d3a8f0
Dhruvit2053 <dhruvitsarvaiya2003@gmail.com>
2025-05-18 21:31:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7336700997:AAEpOdeFlkg1_NILG1chboNo6lGqwmo
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6271337642').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002671004694'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/netflix_movie_times')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+KOd2MPN_-Q01Nzk9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001980263856').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
